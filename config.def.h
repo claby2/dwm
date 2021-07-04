af9e9d63b3c637c7db7cbf0f7433daeb2dcaa776
Edward Wibowo <wibow9770@gmail.com>
2021-07-04 21:10:25 +0800

    feat: change font to fira code

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 20, .gappx = 20};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cascadia Mono:size=12" };
-static const char dmenufont[]       = "Cascadia Mono:size=12";
+static const char *fonts[]          = { "Fira Code:size=12" };
+static const char dmenufont[]       = "Fira Code:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
